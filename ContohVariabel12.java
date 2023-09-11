aa0dceeb037269a35e8159015349265eed324d7d
drm69 <144076551+drm69@users.noreply.github.com>
2023-09-11 12:40:19 +0700

    Add files via upload

---
+++ ContohVariabel12.java
@@ -0,0 +1,12 @@
+
+        String salahSatuHobySayaAdalah = "Bermain petak umpet";
+        Boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte umurSayaSekarang = 20;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai?" + isPandai);
+        System.out.println("Jenis kelamin:" + jenisKelamin);
+        System.out.println("Umurku saat ini:" + umurSayaSekarang);
+        System.out.println(String.format("Saya ber ipk %s, dengan tinggi badan %s", $ipk, tinggi));
+    
