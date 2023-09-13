3ae4a4e4d43d763ca201825966626c056e5a1812
drm69 <yukihira14darma@gmail.com>
2023-09-13 07:36:26 +0700

    Jobsheet3

---
+++ Latihan12.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan12
+ */
+public class Latihan12 {
+
+    public static void main (String[] args){
+        //System.out.println("nama  :xxxxx");
+        //System.out.println("kelas   :xxxxx");
+    }
+}
