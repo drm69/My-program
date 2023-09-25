d212527b61eb26fb6681f9ddca0571057e2329ff
drm69 <yukihira14darma@gmail.com>
2023-09-25 11:39:20 +0700

    update

--- PemilihanPercobaan112.java
+++ PemilihanPercobaan112.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan112 {
     public static void main(String[] args) {
-        Scanner input00 = new Scanner(System.in);
+        Scanner input12 = new Scanner(System.in);
         
         System.out.print("Masukkan angka: ");
-        int angka=input00.nextInt();
+        int angka=input12.nextInt();
 
         if (angka%2==0)
             System.out.println("Angka " + angka +" bilangan genap");
